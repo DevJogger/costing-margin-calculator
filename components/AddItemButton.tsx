f6c0be76ccfd162b0bc3df331b2c707f41eac25b
DevJogger <158173870+DevJogger@users.noreply.github.com>
2024-05-28 16:51:04 -0700

    Update buttons styles

--- components/AddItemButton.tsx
+++ components/AddItemButton.tsx
@@ -6,14 +6,19 @@ interface AddItemButtonProps {
 }
 export default function AddItemButton({ type }: AddItemButtonProps) {
   const addNewItem = useStore((state) => state.addNewItem)
+  const activeTab = useStore((state) => state.activeTab)
 
   return (
     <div className="flex h-16 items-center justify-center text-sm">
       <span
-        className="flex cursor-pointer items-center gap-2 rounded-full border border-stone-500/15 px-6 py-2 capitalize backdrop-brightness-105"
+        className="flex cursor-pointer items-center gap-2 rounded-full border border-stone-500/15 px-6 py-2 capitalize contrast-125"
+        style={{
+          backgroundColor:
+            activeTab === 'cost' ? 'var(--costBg)' : 'var(--priceBg)',
+        }}
         onClick={() => addNewItem(type)}
       >
-        <Plus className='w-4'/> Add New {type}
+        <Plus className="w-4" /> Add New {type}
       </span>
     </div>
   )
--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -6,7 +6,7 @@ export default function Bottom() {
   const triggerResetPopup = useStore((state) => state.triggerResetPopup)
   return (
     <div
-      className="fixed bottom-8 right-4 z-10 grid h-12 w-12 place-items-center rounded-full contrast-150 drop-shadow-[0_-1px_1px_rgba(0,0,0,0.05)]"
+      className="fixed bottom-8 right-4 z-10 grid h-12 w-12 place-items-center rounded-full contrast-125 drop-shadow-[0_-1px_1px_rgba(0,0,0,0.05)]"
       style={{
         backgroundColor:
           activeTab === 'cost' ? 'var(--costBg)' : 'var(--priceBg)',
